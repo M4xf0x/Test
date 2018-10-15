44d1efcc43a67c5aa3b3f94f816655f7b3ae0bfc
M4xf0x <M4xf0x.spam@gmail.com>
2018-10-15 17:32:22 +0200

    Test

---
+++ GiTTest/src/src/de/m4twaily/tp/Main.java
@@ -0,0 +1,37 @@
+package src.de.m4twaily.tp;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerArmorStandManipulateEvent;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class Main extends JavaPlugin implements Listener {
+
+	public void onEnable() {
+		PluginManager pm = Bukkit.getPluginManager();
+
+		pm.registerEvents(this, this);
+	}
+
+	@EventHandler
+	public void onArmorStand(PlayerArmorStandManipulateEvent e) {
+		Player p = e.getPlayer();
+
+		if (!p.isSneaking()) {
+
+			e.setCancelled(true);
+			System.out.println("Hallo");
+			p.sendMessage(" §8>> §c§lLevel gecheatet");
+			p.setLevel(p.getLevel() + 1);
+			p.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 20, 20));
+		} else {
+
+		}
+	}
+
+}
